724d5eb378a5615b4874ae4f72bfcdb8a78b2c32
david hodgetts <david.demainlalune@gmail.com>
2022-08-29 21:33:11 +0200

    fixes localstorage not present on server side rendering
    removes annoying error in the develop terminal output

--- frontend/src/lib/stores/user.store.js
+++ frontend/src/lib/stores/user.store.js
@@ -1,29 +1,32 @@
 import { clearJwt, clearNickname, getJwt, getNickname, storeJwt, storeNickname } from "$lib/services/LocalStorage";
 import { writable } from "svelte/store";
+import { browser } from '$app/env';
 
 function createUserStore() {
 	const { subscribe, set } = writable({
-        nickname: getNickname(),
-        jwt: getJwt(),
+        nickname: browser ? getNickname() : "",
+        jwt: browser ? getJwt() : "",
     });
 
 	return {
 		subscribe,
-
         /**
          * 
          * @param {string} jwt token
          * @param {string} nickname 
          */
         setUser: (jwt, nickname) => {
-            storeJwt(jwt);
-            storeNickname(nickname);
+            if(browser){
+                storeJwt(jwt);
+                storeNickname(nickname);
+            }
             set({nickname, jwt});
         },
-
 		clear: () => {
-            clearJwt();
-            clearNickname();
+            if(browser){
+                clearJwt();
+                clearNickname();
+            }
             set({nickname: "", jwt: ""});
         }
 	};
