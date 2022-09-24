fa79c2fd6a0ab077ba06167214533d0b5159e455
david hodgetts <david.demainlalune@gmail.com>
2022-09-24 16:39:43 +0200

    udpates strapi to v4.3.9

--- backend/dist/config/server.js
+++ backend/dist/config/server.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
+    url: "https://api.permacriture.org",
     app: {
         keys: env.array('APP_KEYS'),
     },
