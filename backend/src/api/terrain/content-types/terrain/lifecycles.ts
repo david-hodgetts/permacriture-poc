23b40c49aae784c93a24e01a180f2059c2fd967b
david hodgetts <david.demainlalune@gmail.com>
2022-10-05 11:47:24 +0200

    extends Terrain model

---
+++ backend/src/api/terrain/content-types/terrain/lifecycles.ts
@@ -0,0 +1,24 @@
+
+const { ValidationError } = require("@strapi/utils").errors;
+function validateTerrain(event){
+    const start =  new Date(event.params.data.start);
+    const end =  new Date(event.params.data.end);
+
+    // end must be greater than start
+    if(end.getTime() <= start.getTime()){
+        // ref -> https://forum.strapi.io/t/throw-error-message-in-lifecycle-hook-doesnt-change-the-error-message-display-in-the-ui/1515/14
+        throw new ValidationError("end must be set after start");  
+    }
+}
+
+export default {
+  beforeCreate(event) {
+    validateTerrain(event);
+  },
+  beforeUpdate(event) {
+    validateTerrain(event);
+  },
+  beforeUpdateMany(event) {
+    validateTerrain(event);
+  }
+}
