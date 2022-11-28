3efa386b8b781f418f7e968e68891074c97caccd
David Hodgetts <david.demainlalune@gmail.com>
2022-11-28 11:26:50 +0100

    adds scss support

---
+++ frontend/src/lib/components/Navigation.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<nav>
+    <h1>bou</h1>
+</nav>
+
+
+<style lang="scss">
+    h1{
+        font-size: var(--font-size);
+    }
+</style>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 // does not seem to work as intended...
-export const csr = false;
-export const prerender = true;
-export const ssr = false;
+// export const csr = false;
+// export const prerender = true;
+// export const ssr = false;
+import Navigation from "$lib/components/Navigation.svelte";
 </script>
+
+<Navigation />
 
 <slot></slot>
