a0e28db1baa5659c777e6bff542ed11eb9fc024b
David Hodgetts <david.demainlalune@gmail.com>
2023-02-26 17:03:34 +0100

    increases default rest limit (strapi)

--- backend/dist/config/api.js
+++ backend/dist/config/api.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     rest: {
-        defaultLimit: 25,
-        maxLimit: 100,
+        defaultLimit: 500,
+        maxLimit: 1000,
         withCount: true,
     },
 };
