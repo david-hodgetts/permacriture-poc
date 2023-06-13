aa166c9fa662c273a402f6c2bc9f2c3d4f30de90
David Hodgetts <david.demainlalune@gmail.com>
2023-06-13 18:57:23 +0200

    [backend] adds route to get author list

--- backend/dist/src/api/author/controllers/author.js
+++ backend/dist/src/api/author/controllers/author.js
@@ -4,4 +4,30 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::author.author');
+// export default factories.createCoreController('api::author.author');
+exports.default = strapi_1.factories.createCoreController('api::author.author', ({ strapi }) => ({
+    // Method 2: Wrapping a core action (leaves core logic in place)
+    async find(ctx) {
+        const userId = ctx.state.user.id;
+        let userContext;
+        try {
+            userContext = await strapi.service('api::user-context.user-context').getContext(userId);
+        }
+        catch (e) {
+            return ctx.badRequest("invalid user context", {});
+        }
+        console.log("usercontext", userContext);
+        const authors = await strapi.db.query('api::author.author').findMany({
+            select: ['id', 'nickname'],
+            where: {
+                'terrain': {
+                    'id': userContext.author.terrain.id,
+                },
+            }
+        });
+        console.log("authors", authors);
+        ctx.body = {
+            data: authors
+        };
+    },
+}));
--- backend/src/api/author/controllers/author.ts
+++ backend/src/api/author/controllers/author.ts
@@ -4,4 +4,37 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::author.author');
+// export default factories.createCoreController('api::author.author');
+
+export default factories.createCoreController('api::author.author', ({ strapi }) => ({
+
+    // Method 2: Wrapping a core action (leaves core logic in place)
+    async find(ctx) {
+        const userId = ctx.state.user.id;
+        let userContext;
+        try{
+            userContext = await strapi.service('api::user-context.user-context').getContext(userId);
+        }catch (e){
+            return ctx.badRequest("invalid user context", {});
+        }
+        console.log("usercontext", userContext);
+
+
+        const authors = await strapi.db.query('api::author.author').findMany({
+            select: ['id', 'nickname'],
+            where:{
+                    'terrain': {
+                        'id': userContext.author.terrain.id,
+                    },
+                }
+            },
+        );
+        
+        
+        console.log("authors", authors);
+
+        ctx.body = {
+            data: authors 
+        };
+    },
+}));
