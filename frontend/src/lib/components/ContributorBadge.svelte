83d9fab5b942f4fbea5b71e7f7a323cf9051115d
David Hodgetts <david.demainlalune@gmail.com>
2023-10-28 15:25:00 +0200

    adds ContributorBadge component

---
+++ frontend/src/lib/components/ContributorBadge.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { BadgeSize } from "$lib/components-utils/Badge";
+
+    export let color="#ffffff";
+    export let text = "";
+    export let size:BadgeSize = BadgeSize.Big;
+
+    let sizePx = (size == BadgeSize.Big ? 37 : 22);
+
+    let fontSize = (size == BadgeSize.Big ? 15 : 10);
+ 
+
+</script>
+
+<div 
+    class="badge"
+    class:small={size == BadgeSize.Small} 
+    style="background-color: {color}; width: {sizePx}px; height: {sizePx}px;"
+>
+    <div 
+        class="text"
+        style="font-size: {fontSize}px;"
+    >
+        {text.toUpperCase()}
+    </div>
+</div>
+
+<style>
+    .badge{
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        filter: drop-shadow(0px 3px 6px #00000029);
+    }    
+
+    .text{
+        font-family: Luminari;
+        color: white;
+    }
+</style>
---
+++ frontend/src/routes/test/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { BadgeSize } from "$lib/components-utils/Badge";
+    import ContributorBadge from "$lib/components/ContributorBadge.svelte";
+</script>
+
+<div class="container">
+
+    <h1>test page</h1>
+
+    <ContributorBadge color="#ffaa00" text="DH"/>
+    <ContributorBadge color="#00aaff" size={BadgeSize.Small} text="AR"/>
+</div>
+
+<style>
+    .container{
+        margin-left: 40px;
+    }
+</style>
