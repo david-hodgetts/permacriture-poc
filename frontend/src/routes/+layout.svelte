00f115da5407b406064bc009181760ba7858196f
David Hodgetts <david.demainlalune@gmail.com>
2023-11-28 15:06:15 +0100

    center and limit width of main element

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,25 +1,37 @@
 <script>
-// does not seem to work as intended...
-// export const csr = false;
-// export const prerender = true;
-// export const ssr = false;
-import Notifications from 'svelte-notifications';
-import Navigation from "$lib/components/Navigation.svelte";
-import "../style/app.scss";
+    // does not seem to work as intended...
+    // export const csr = false;
+    // export const prerender = true;
+    // export const ssr = false;
+    import Notifications from 'svelte-notifications';
+    import Navigation from "$lib/new-components/Navigation.svelte";
+    import "../style/app.scss";
+    import { page } from "$app/stores";
+
+    $: isMapPage = $page.url.pathname.startsWith("/map") ;
+
+
 </script>
 
-<!-- <Notifications>
+<Notifications>
     <Navigation />
-    <main>
+    <main class:map-page={isMapPage}>
         <slot></slot>
     </main>
-</Notifications> -->
+</Notifications>
 
-<slot></slot>
 
 <style lang="scss">
     main{
         margin-top: var(--navbar-height);
         height: 100vh;
+        max-width: 700px;
+        margin-left: var(--app-col-margin);
+        margin-right: var(--app-col-margin);
     }
+
+    .map-page{
+        margin-top: var(--navbar-height-map);
+    }
+
 </style>
