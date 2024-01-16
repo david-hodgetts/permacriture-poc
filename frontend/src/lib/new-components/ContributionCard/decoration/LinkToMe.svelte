43fe0ee6fbcf6fe89ab89f47e83edcb6dc1d57b1
David Hodgetts <david.demainlalune@gmail.com>
2024-01-16 10:38:59 +0100

    [frontend] *se perlier* becomes *perlier*

--- frontend/src/lib/new-components/ContributionCard/decoration/LinkToMe.svelte
+++ frontend/src/lib/new-components/ContributionCard/decoration/LinkToMe.svelte
@@ -7,7 +7,7 @@
 <div class="link-to-contribution no-select" on:click on:keydown={() => null} role="button" tabindex=0>
     <!-- <img src="/images/linkToMe.svg" alt=""> -->
     <LinkToMeIcon isGraine={isGraine} />
-    <div class="text" class:graine={isGraine}>se perlier</div>
+    <div class="text" class:graine={isGraine}>perlier</div>
 </div>
     
 <style>
