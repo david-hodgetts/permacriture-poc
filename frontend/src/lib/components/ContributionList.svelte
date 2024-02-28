472dd437d587c3d2576722297c531d532c5ac44a
David Hodgetts <david.demainlalune@gmail.com>
2024-02-28 10:38:16 +0100

    adds message when contribution list is empty
    message encourages user to add a new contribution to start the
    experience

--- frontend/src/lib/components/ContributionList.svelte
+++ frontend/src/lib/components/ContributionList.svelte
@@ -7,12 +7,20 @@
 </script>
 
 <div class="contribution-list">
-    {#each contributions as contribution (contribution.id) }
-        <ContributionCard 
-            contribution={contribution} 
-            on:showDetailRequest
-        />
-    {/each}
+    {#if contributions.length > 0}
+        {#each contributions as contribution (contribution.id) }
+            <ContributionCard 
+                contribution={contribution} 
+                on:showDetailRequest
+            />
+        {/each}
+    {:else}
+        <div class="no-content">
+            Vous n'avez pas encore rédigé de texte.<br>
+            Passez sur la vue <u>tous les textes</u> et sélectionner <u>esperlier</u> 
+            sur un texte qui vous intéresse pour&nbsp;commencer.
+        </div>
+    {/if}
     <Footer />
 </div>
 
@@ -26,5 +34,12 @@
         padding-top: 20px;
         padding-top: 40px;
         // overflow-y: scroll;
+    }
+
+    .no-content{
+        padding-top: 40px;
+        padding-bottom: 10px;
+        text-align: center;
+        line-height: 1.3em;
     }
 </style>
