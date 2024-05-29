85b272e1d30e8e9457eb45a44344a2d48e5573f1
David Hodgetts <david.demainlalune@gmail.com>
2024-05-29 11:28:58 +0200

    refactor(backend): add proper types to authorize function

--- backend/src/api/context/controllers/context.ts
+++ backend/src/api/context/controllers/context.ts
@@ -1,12 +1,12 @@
 
 import { factories } from '@strapi/strapi'
 import { UserContext } from '../../user-context/services/user-context';
-import { authorize } from '../../../lib/authHelper';
+import { TerrainContext, authorize } from '../../../lib/authHelper';
 
 export default factories.createCoreController('api::contribution.contribution', ({ strapi }) => ({
 
     async find(ctx) {
-        let terrainContext;
+        let terrainContext: TerrainContext;
         try{
             terrainContext = await authorize(strapi, ctx);
         }catch(e){
